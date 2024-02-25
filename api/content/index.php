40478fd4490cee2ae031d189ce36565702fca3f2
Vikorion <viktorvikorion@gmail.com>
2024-02-25 20:19:59 +0100

    kimaradt content/index.php ben is átírásra került a conn.php -> ../connection/index.php

--- api/content/index.php
+++ api/content/index.php
@@ -4,7 +4,7 @@
         $keres = $_POST['keresadat'];
 
         require '../connection/index.php';
-        $query = mysqli_query($conn, "SELECT * FROM `recept` WHERE rcim LIKE '%$keres%'") or die(mysqli_error());
+        $query = mysqli_query($conn, "SELECT * FROM `recept` WHERE rcim LIKE '%$keres%'") or die(mysqli_error($conn));
         while($fetch = mysqli_fetch_array($query)){
             echo $fetch['rcim'];
             echo $fetch['rhozzavalok'];
