98d30a59c71a5312108f3a13099e949673ec605c
Vikorion <viktorvikorion@gmail.com>
2024-03-13 17:55:08 +0100

    duplikáció kizárása a regisztráció elött

--- api/createuser/index.php
+++ api/createuser/index.php
@@ -1,43 +1,59 @@
 <?php
+//Create user hozzáadása az adatbázishoz:
 
 header('Content-Type: application/json; charset=utf-8');
-//Create
-//user hozzáadása az adatbázishoz:
 
-// ha ellenőrzés megtörténik, akkor mehet a db-be:
+// fő logika, az eredmény kimenetekkel:
 if (check_fields()) {
-
-    //ellenőrzés, hogy létezik-e már a felhsználó
-    //require 'findUser.php';
-
-    /*a user ellenőrzés során létezik-e a felhasználó, 
-        ha igen, akkor FALSE-val tér vissza azaz nem íródik az adatbázisba, 
-        ha nem, akkor lefut az adatbázisba bejegyzés és TRUE adodik vissza.*/
-    //if (find_user()) {
-    //    return false;
-    //} else {
+    if (free_user()) {
         user_create();
         $createuser = [
-            'hiba'          => 0,
-            'unick'         => $_POST["unick"],
-            'umail'         => $_POST["umail"],
-            'regtime'       => date('Y-m-d H:i:s')
+            ['hiba'          => 0],
+            [
+                'unick'         => $_POST["unick"],
+                'umail'         => $_POST["umail"],
+                'regtime'       => date('Y-m-d H:i:s')
+            ]
         ];
-    //}
+    } else {
+        $createuser = ['hiba' => "Létezik már ilyen regiaztráció!"];
+    }
 } else {
     $createuser = ['hiba' => "Nincs kitöltve megfelelően a regisztráció"];
 }
 
-// ellenőrzése, hogy minden mező ki van-e töltve. szuperglobális($_POST) be van-e állítva és nem üres, azaz be van állítva valami? Később, az adatok részletesebb ellenőrzése is itt valósulhat meg. 
-function check_fields(){
-
+// ellenőrzése, hogy minden mező ki van-e töltve. szuperglobális($_POST) be van-e állítva és nem üres, azaz be van állítva valami?
+function check_fields()
+{
     $unick = isset($_POST["unick"]) && !empty($_POST["unick"]);
     $umail = isset($_POST["umail"]) && !empty($_POST["umail"]);
     $upw = isset($_POST["upw"]) && !empty($_POST["upw"]);
     return $unick && $umail && $upw;
 }
+// szabad-e a felhasználónév, illetve emailcím.
+function free_user()
+{
+    $unick = $_POST["unick"];
+    $umail = $_POST["umail"];
 
-function user_create(){
+    //adatbázis kapcsolat felépítése
+    require '../connection/index.php';
+
+    $search = mysqli_query($conn, "SELECT unick, umail FROM user WHERE unick = '$unick' OR umail = '$umail'");
+    $row = mysqli_fetch_assoc($search);
+
+    mysqli_close($conn);
+
+    if (!isset($row)) {
+        return TRUE;
+    } elseif ($row['unick'] == $unick || $row['umail'] == $umail) {
+        $finduser = ['hiba' => "Létezik már ilyen regiaztráció!"];
+        return FALSE;
+    };
+}
+// felhasználó beírása az adatbázisba.
+function user_create()
+{
 
     //a POST függvény kiolvasása, a htmlspecialchars() segítségével bizonyos karakterek ", ', <, >  kikódolásával:
     $unick = htmlspecialchars($_POST["unick"]);
@@ -52,15 +68,12 @@ function user_create(){
 
     $stmt_prepare = mysqli_prepare($conn, $stmt);
 
-    mysqli_stmt_bind_param($stmt_prepare, "sss", $unick, $umail, $upw); 
+    mysqli_stmt_bind_param($stmt_prepare, "sss", $unick, $umail, $upw);
 
     mysqli_stmt_execute($stmt_prepare);
 
     mysqli_close($conn);
-
 }
 
 $json = json_encode($createuser, JSON_UNESCAPED_UNICODE);
 print $json;
-
-?>
