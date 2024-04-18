a44fdbb541e664186dd64d14ab723ff580486b4d
Vikorion <68995461+Vikorion@users.noreply.github.com>
2024-04-18 18:23:44 +0200

    Publikus profilváltás miatt kiszedtem a felhasználó nevet és jelszavát

--- api/connection/index.php
+++ api/connection/index.php
@@ -1,8 +1,8 @@
 <?php
 
 $hostname = "localhost";
-$username = "recept24";
-$password = "recept24";
+$username = "";
+$password = "";
 $database = "recept24";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
